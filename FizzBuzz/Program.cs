afc9a22f73d2b051cf48032c3dddfe819862fa2e
Michael Clemons <Mclemons@tql.com>
2024-06-13 14:12:32 -0400

    made more efficent

--- FizzBuzz/Program.cs
+++ FizzBuzz/Program.cs
@@ -4,40 +4,16 @@
     {
         static void Main(string[] args)
         {
-            int[] nmbrs = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25,
-            26, 27, 28, 29, 30};
-
-            foreach (int n in nmbrs)
+            for (int n = 1; n < 30; n++) //use for loop to iterate through the number 1-30 
             {
-                string Fizz = (n % 3 == 0 && n % 5 == 0) ? "FIZZBUZZ"
-                            : (n % 5 == 0 && n % 3 != 0 ? "BUZZ"
-                            : (n % 3 == 0 && n % 5 != 0 ? "FIZZ"
-                            : n.ToString()));
+                string Fizz = (n % 3 == 0 && n % 5 == 0) ? $"{n}: FIZZBUZZ" 
+                            : (n % 5 == 0 ? $"{n}: BUZZ" 
+                            : (n % 3 == 0 ? $"{n}: FIZZ"
+                            : n.ToString() ));
 
-                Console.Write(Fizz + ", ");
+                Console.Write(Fizz + ", ");  
             }
-
-            //int[] ints = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25 };
-
-            //    int i = 0;
-            //    int cubed = 0;
-
-
-            //while (true) 
-            //{
-
-            //    if (ints[i] % 2 != 0)
-            //    {
-
-            //        cubed = Math.Pow(ints[i], 3);
-            //        Console.WriteLine(cubed);
-
-            //    } 
-            //        i++;
-
-            //    if (i >= 25) { break; }
-            //}   
-
         }
     }
 }
+
